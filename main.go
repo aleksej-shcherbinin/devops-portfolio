8ff66c9f3fa048e46babc016b0e02d947561314e
aleksej-shcherbinin <96979729+aleksej-shcherbinin@users.noreply.github.com>
2022-01-03 19:15:01 +0500

    Add files

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	port := os.Getenv("PORT") // 80
+	if strings.TrimSpace(port) == "" {
+		log.Println("PORT environment variable has not been set")
+		port = "80"
+	}
+
+	redirectTo := os.Getenv("TO")     // https://google.com
+	if strings.TrimSpace(redirectTo) == "" {
+		log.Println("TO environment variable has not been set")
+		redirectTo = "https://google.com"
+	}
+
+	redirectType := os.Getenv("TYPE") // 301
+
+	redirectCode, err := strconv.Atoi(redirectType)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check of type is 301 or 302 or 303
+	if redirectCode != http.StatusMovedPermanently &&
+		redirectCode != http.StatusFound &&
+		redirectCode != http.StatusSeeOther {
+		err := fmt.Errorf("Forwarding type %s is incorrect", redirectType)
+		log.Fatal(err)
+	}
+
+	log.Printf("Redirect to %s with type %d", redirectTo, redirectCode)
+
+	http.HandleFunc(
+		"/",
+		func(w http.ResponseWriter, r *http.Request) {
+			http.Redirect(w, r, redirectTo, redirectCode)
+		},
+	)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
